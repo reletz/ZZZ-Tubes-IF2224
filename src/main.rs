c347e942eab6ed6c38ff86f91d8aabad3666d3bb
reletz <naufarrelzhafifabhista@gmail.com>
2025-10-10 17:29:50 +0700

    feat: parsing pascal-s' content

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use std::env;
+use std::fs;
 use std::process;
 
 mod lexer;
-mod parser;
-mod semantic_analyzer;
-mod code_generator;
-mod utils;
-mod interpreter;
-
-use lexer::lexer::PascalLexer;
 
 fn main() {
+    let args:Vec<String> = env::args().collect();
+    if args.len() < 2{
+        eprintln!("Usage: cargo run -- <source_file.pas>");
+        process::exit(1);
+    }
 
+    let file_path = &args[1];
+
+    let source_code = match fs::read_to_string(file_path){
+        Ok(code) => code,
+        Err(e) => {
+            eprintln!("Error reading file '{}': {}", file_path, e);
+            process::exit(1);
+        }
+    };
+
+    println!("Source code:\n{}", source_code);
 }
