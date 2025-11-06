842485d4a9047951787a17625343ce8d39251d3f
susTuna <hidasetsuna@gmail.com>
2025-11-06 17:46:14 +0700

    feat: statement parsing fun (most r still placeholders) feat: if, while statements done

--- src/parser/statements.rs
+++ src/parser/statements.rs
@@ -34,15 +34,58 @@ impl PascalParser {
 
 	/// Placeholder
 	pub(super) fn parse_statement(&mut self) -> Result<Statement, SyntaxError> {
-		// TODO: Implementasi ini
-		// if self.check_keyword("jika") { self.parse_if_statement() } ...
 		
-		// For now, treat any expression as a statement (incomplete, but works for testing)
-        if self.check(TokenType::Identifier) {
+		if self.check_keyword("jika") {
+			self.parse_if_statement()
+		} else if self.check_keyword("selama") {
+			self.parse_while_statement()
+		} else if self.check_keyword("untuk") {
+			self.parse_for_statement()
+		} else if self.check_keyword("ulangi") {
+			self.parse_repeat_statement()
+		} else if self.check_keyword("kasus") {
+			self.parse_case_statement()
+		} else if self.check_keyword("mulai") {
+			self.parse_compound_statement()
+		} else if self.check(TokenType::Identifier) { // For now, treat any expression as a statement (incomplete, but works for testing)
             let expr = self.parse_expression()?;
             return Ok(Statement::ExpressionStatement(expr));
-        }
-        
-        Err(self.error("Expected statement"))
+        } else {
+			Err(self.error("Expected statement"))
+		}
     }
+
+	fn parse_if_statement(&mut self) -> Result<Statement, SyntaxError> {
+		self.consume_keyword("jika", "Expected 'jika'.")?;
+		let condition = self.parse_expression()?;
+		self.consume_keyword("maka", "Expected 'maka' after condition.")?;
+		let then_branch = Box::new(self.parse_statement()?);
+
+		let else_branch = if self.check_keyword("selain-itu") {
+			self.advance();
+			Some(Box::new(self.parse_statement()?))
+		} else {
+			None
+		};
+
+		Ok(Statement::If(IfStatement {
+			condition,
+			then_branch, 
+			else_branch
+		}))
+	}
+
+	fn parse_while_statement(&mut self) -> Result<Statement, SyntaxError> {
+		self.consume_keyword("selama", "Expected 'selama'.")?;
+		let condition = self.parse_expression()?;
+		self.consume_keyword("lakukan", "Expected 'lakukan' after condition.")?;
+		let body = Box::new(self.parse_statement()?);
+
+		Ok(Statement::While(WhileStatement {
+			condition,
+			body
+		}))
+	}
+
+	fn 
 }
